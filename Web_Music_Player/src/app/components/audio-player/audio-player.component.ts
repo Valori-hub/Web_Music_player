385275b652d16466ac89e8d98e24dbd2af51575a
Adam_Elcner <mradam621@gmail.com>
2024-05-07 15:09:28 +0200

    just for a moment

--- Web_Music_Player/src/app/components/audio-player/audio-player.component.ts
+++ Web_Music_Player/src/app/components/audio-player/audio-player.component.ts
@@ -1,9 +1,12 @@
-import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
+import { Component, Inject, PLATFORM_ID, ChangeDetectorRef } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
 import {MatIconModule} from '@angular/material/icon';
 import {MatGridListModule} from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import {MatSliderModule} from '@angular/material/slider';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-audio-player',
@@ -13,25 +16,37 @@ import {MatSliderModule} from '@angular/material/slider';
     MatGridListModule,
     MatCardModule,
     MatSliderModule,
+    CommonModule,
+    FormsModule
   ],
   templateUrl: './audio-player.component.html',
   styleUrl: './audio-player.component.scss'
 })
 export class AudioPlayerComponent{
+  sliderValue: number = 0
   private audio!: HTMLAudioElement;
-
-  constructor(@Inject(PLATFORM_ID) private platformId: Object) {
+  isPlaying: boolean = false;
+  constructor(@Inject(PLATFORM_ID) private platformId: Object, private changeDetectorRef: ChangeDetectorRef) {
     if (isPlatformBrowser(this.platformId)) {
       this.audio = new Audio();
     }
   }
-
-  openSong() {
-    this.audio.src = "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/chasing-that-feeling.mp3";
-    this.audio.load();
-    this.audio.play();
+  toggleMusic() {
+    this.isPlaying = !this.isPlaying;
+    this.changeDetectorRef.detectChanges();
+    if (this.isPlaying) {
+      this.audio.src = "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/chasing-that-feeling.mp3";
+      this.audio.load();
+      this.audio.play();
+      console.log(this.audio.currentTime);
+      }else{
+        this.audio.pause();
+      }
     }
-  pauseSong(){
-    this.audio.pause();
+    changeVolume(event: any) {
+      const volume: any = event.value;
+      console.log(parseFloat(volume));
+    }
+
   }
-}
+  
